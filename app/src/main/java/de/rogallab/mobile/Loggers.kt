69d027553f84b1ed8cb10faa50738e6e401a7f72
BerndR <bu.rogalla@icloud.com>
2023-10-02 17:08:11 +0200

    Loggers included

--- app/src/main/java/de/rogallab/mobile/Loggers.kt
+++ app/src/main/java/de/rogallab/mobile/Loggers.kt
@@ -1,23 +1,8 @@
 package de.rogallab.mobile
 
 import android.util.Log
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.remember
 import de.rogallab.mobile.MainActivity.Companion.isDebug
 import de.rogallab.mobile.MainActivity.Companion.isInfo
-
-
-@Composable
-fun LogComp(tag: String, msg: String) {
-   if (isDebug) {
-      val ref = remember { Ref(1) }
-      SideEffect { ref.value++ }
-      Log.d(tag, "Compositions ${ref.value}: $msg")
-   }
-}
-
-class Ref(var value: Int)
 
 fun logError(tag: String, message: String) {
    val msg = formatMessage(message)
