4d562f698cf7cf813804814df01cb8911f101a28
rogallab <b-u.rogalla@ostfalia.de>
2023-10-08 13:49:16 +0200

    Libraries updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
 
    // https://developer.android.com/jetpack/androidx/releases/activity
-   val activityCompose = "1.7.2"
+   val activityCompose = "1.8.0"
    implementation("androidx.activity:activity-compose:$activityCompose")
 
    // https://developer.android.com/jetpack/androidx/releases/core
@@ -67,12 +67,12 @@ dependencies {
    // A BOM is a Maven module that declares a set of libraries with their versions.
    // It will greatly simplify the way you define Compose library versions in your
    // Gradle dependencies block.
-   implementation(platform("androidx.compose:compose-bom:2023.09.00"))
+   implementation(platform("androidx.compose:compose-bom:2023.10.00"))
    implementation("androidx.compose.ui:ui")
    implementation("androidx.compose.ui:ui-graphics")
    implementation("androidx.compose.ui:ui-tooling-preview")
-   implementation("androidx.compose.material3:material3")
-
+   val material3 = "1.1.2"
+   implementation("androidx.compose.material3:material3:$material3")
 
    // TESTS -----------------------
    testImplementation("junit:junit:4.13.2")
@@ -102,8 +102,9 @@ dependencies {
    androidTestImplementation("androidx.test:runner:$runner")
 
    // To use Compose Testing
-   androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.00"))
+   androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
    debugImplementation("androidx.compose.ui:ui-tooling")
-   debugImplementation("androidx.compose.ui:ui-test-manifest")
+   val uiTestManifest = "1.5.3"
+   debugImplementation("androidx.compose.ui:ui-test-manifest:$uiTestManifest")
 }
